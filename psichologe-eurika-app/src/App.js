4abac8b4903c78ec249d607c9671df68dfde2e39
MartynasGen <mgenzuras@gmail.com>
2020-01-10 14:05:29 +0200

    Clean up

--- psichologe-eurika-app/src/App.js
+++ psichologe-eurika-app/src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Galingasis Eurikos puslapis!
-        </p>
-      </header>
+      Eurikos puslapis
     </div>
   );
 }
